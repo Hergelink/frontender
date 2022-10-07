a8f72214828a281c4f013ff5cd9bd1e97eaea61c
Hergelink <enver.hergelink@gmail.com>
2022-10-07 17:12:55 +0300

    improved the error page css

--- src/components/Main-Components/ErrorPage.jsx
+++ src/components/Main-Components/ErrorPage.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "../Styles/ErrorPage.css"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../Styles/ErrorPage.css';
 
 function ErrorPage() {
   return (
     <div className='error-page-container'>
-      <div className="error-info-container">
-      <div className="err-emoji-div">🤔</div>
-      <h1>Page not found</h1>
-      <p>The page you are trying to access does not exist. Maybe the buttons bellow can help.</p>
-      <Link to="/">Home</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/faq">FAQ</Link>
-      <Link to="/contact">Contact</Link>
+      <div className='error-info-container'>
+        <div className='err-emoji-div'>🤔</div>
+        <h1>Page not found</h1>
+        <p>
+          The page you are trying to access does not exist. Maybe the buttons
+          bellow can help.
+        </p>
+        <div className='links-container'>
+          <Link to='/'>Home</Link>
+          <Link to='/projects'>Projects</Link>
+          <Link to='/faq'>FAQ</Link>
+          <Link to='/contact'>Contact</Link>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;
