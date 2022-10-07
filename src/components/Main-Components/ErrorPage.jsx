8379d6a14b446ca1524139a6a43dd04317a7e88f
Enver Can Oude Hergelink <enver.hergelink@gmail.com>
2022-10-07 16:32:42 +0300

    added the error page

--- src/components/Main-Components/ErrorPage.jsx
+++ src/components/Main-Components/ErrorPage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import "../Styles/ErrorPage.css"
 
 function ErrorPage() {
   return (
-    <div>Error! - The page you are looking for does not exist.</div>
+    <div className='error-page-container'>
+      <div className="error-info-container">
+      <div className="err-emoji-div">🤔</div>
+      <h1>Page not found</h1>
+      <p>The page you are trying to access does not exist. Maybe the buttons bellow can help.</p>
+      <Link to="/">Home</Link>
+      <Link to="/projects">Projects</Link>
+      <Link to="/faq">FAQ</Link>
+      <Link to="/contact">Contact</Link>
+      </div>
+    </div>
   )
 }
 
